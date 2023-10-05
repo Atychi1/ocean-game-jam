0856643201c4b8074e86c0fefe2eb8d7408aba47
Atychi1 <platinumninja44@gmail.com>
2023-10-05 20:28:01 +0100

    saving scene

---
+++ Game Jam/Assets/Enemy.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public static event Action<Enemy> OnEnemyKilled; //this makes a fun counter that shows you when the enemy's killed!
+    [SerializeField] float health, maxHealth = 100f;
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        health = maxHealth;
+    }
+
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+            OnEnemyKilled?.Invoke(this);
+        }
+        
+
+    }
+}
